f89f6d9b7e9dee44275584206d63f9d35dbb646a
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-04-02 18:12:25 +0100

    corrected name for kocak...

--- data/kokac/list-of-pubs-proc.sh
+++
@@ -1,5 +0,0 @@
-# to DOI
-cat list-of-publications.txt | grep doi | sort | grep -oE 'doi.org/[0-9\.\/a-zA-Z\-]+' > dois.txt
-
-# to pub_names
-cat list-of-publications.txt | grep doi | sort | cut -d ')' -f 2-10 | cut -d . -f 1 | sed 's/^ //' > pub_names.txt 
