95c23dfade98ff8c141d37dab113bd58ee4a8d3d
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-04-11 10:10:25 +0100

    copying files as well

--- src/make_static_app.py
+++ src/make_static_app.py
@@ -1,8 +1,12 @@
+from genericpath import isdir
+
+
 SOURCE_DIR = "src/static_app_templates"
 
 if __name__ == "__main__":
     import os
     import json
+    import shutil
     from ratings import export_model_to_json
     from prompt import make_prompt
     from metrics import load_config
@@ -12,6 +16,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate static app")
     parser.add_argument("--config_path", default="config.yaml")
     parser.add_argument("--output_dir", default="static_app")
+    parser.add_argument("--additional_files_to_copy", default=None, nargs="+")
     args = parser.parse_args()
 
     items, conditions = load_config(args.config_path)
@@ -36,3 +41,13 @@ if __name__ == "__main__":
         f.write(main_html)
     with open(f"{args.output_dir}/main.js", "w") as f:
         f.write(js_file)
+
+    if args.additional_files_to_copy:
+        for file in args.additional_files_to_copy:
+            if os.path.isdir(file):
+                dir_name = os.path.basename(file)
+                shutil.copytree(
+                    file, f"{args.output_dir}/{dir_name}", dirs_exist_ok=True
+                )
+            else:
+                shutil.copy(file, args.output_dir)
