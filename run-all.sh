b0531f4751367411e9699965c403c2a2a87f1ffe
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-04-29 12:58:59 +0100

    added more models

--- run-all.sh
+++ run-all.sh
@@ -1,7 +1,14 @@
 source .env
 
 declare -a CONFIGS=("config.yaml" "config-enhanced.yaml")
-declare -a OLLAMA_MODELS=("dolphin3:8b-llama3.1-q4_K_M" "llama3.2:3b-instruct-q4_K_M" "qwen2.5:3b-instruct" "qwen2.5:7b-instruct")
+declare -a OLLAMA_MODELS=(
+    "gemma3:4b-it-q4_K_M"
+    "gemma3:12b-it-q4_K_M"
+    "llama3.2:3b-instruct-q4_K_M"
+    "qwen3:4b-q4_K_M"
+    "qwen3:8b-q4_K_M"
+    "mistral:7b-instruct-q4_0"
+    "phi4:14b-q4_K_M")
 ARTICLE_PATH=article-text
 OUTPUT_PATH=ratings
 
@@ -20,9 +27,9 @@ do
         do 
             text_name=$(basename $text | sed 's/\.txt//')
             echo Analysing $text_name with $config
-            uv run python src/rate.py \
+            echo uv run python src/rate.py \
                 --article_path "$text" \
-                --output_path $OUTPUT_PATH/gemini/$set_name/$text_name.json \
+                --output_path "$OUTPUT_PATH/gemini/$set_name/$text_name.json" \
                 --config_path $config &
         done
     done
