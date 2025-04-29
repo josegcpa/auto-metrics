9ec68f0267304472f4f68cc6c4e39a7bfde87d4a
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-04-29 18:44:25 +0100

    unifying interface for requests

--- src/rate.py
+++ src/rate.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     import json
     import time
     from pathlib import Path
-    from tqdm import tqdm
 
     from metrics import load_config
     from ratings import export_model_to_json
@@ -40,98 +39,26 @@ if __name__ == "__main__":
 
     start_time = time.time()
     if args.local_model is None:
-        from google import genai
-        from google.genai.types import GenerateContentConfig
+        from src.request_utils.gemini_request import query_model
 
-        config = GenerateContentConfig(
-            temperature=TEMPERATURE,
-            top_p=TOP_P,
-            seed=SEED,
-            response_mime_type="application/json",
-            response_schema=data_model,
-        )
-
-        client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
-        chat = client.chats.create(model="gemini-2.0-flash-001")
-
-        response = chat.send_message(
-            message=prompt.format(article=article_text), config=config
-        )
-        response = response.text
+        response = query_model(prompt.format(article=article_text), data_model)
     else:
         split_model = args.local_model.split(":")
         provider, model_str = split_model[0], ":".join(split_model[1:])
         if provider == "ollama":
-            from ollama import Client
+            from request_utils.ollama_request import query_model
 
-            client = Client()
-            response_stream = client.generate(
-                prompt=prompt.format(article=article_text),
-                model=model_str,
-                options={
-                    "top_p": TOP_P,
-                    "seed": SEED,
-                    "temperature": TEMPERATURE,
-                },
-                format=export_model_to_json(data_model),
-                stream=True,
+            response = query_model(
+                query=prompt.format(article=article_text),
+                data_model=export_model_to_json(data_model),
+                model_str=model_str,
             )
-            response = ""
-            for content in tqdm(response_stream):
-                response += content["response"]
         elif provider == "huggingface":
-            import outlines
-            import torch
-            from transformers import (
-                AutoModelForCausalLM,
-                AutoTokenizer,
-            )
-            from outlines.models.transformers import Transformers
-            from outlines.generate.api import (
-                GenerationParameters,
-                SamplingParameters,
-            )
-            from transformers import set_seed
+            from src.request_utils.huggingface_request import query_model
 
-            if torch.cuda.is_available():
-                device = "cuda"
-            elif torch.mps.is_available():
-                device = "mps"
-            else:
-                device = "cpu"
-
-            set_seed(SEED)
-
-            model = AutoModelForCausalLM.from_pretrained(
-                model_str,
-                device_map=device,
-                torch_dtype="auto",
-                trust_remote_code=True,
+            response = query_model(
+                prompt.format(article=article_text), data_model, model_str
             )
-            tokenizer = AutoTokenizer.from_pretrained(model_str)
-            outlines_tokenizer = outlines.models.TransformerTokenizer(tokenizer)
-
-            processor = outlines.processors.JSONLogitsProcessor(
-                data_model, tokenizer=outlines_tokenizer
-            )
-            sampling_parameters = SamplingParameters(
-                sampler="greedy",
-                top_p=TOP_P,
-                temperature=TEMPERATURE + 1e-6,
-            )
-
-            outlines_model = Transformers(model=model, tokenizer=tokenizer)
-            response_stream = outlines_model.stream(
-                prompt.format(article=article_text),
-                generation_parameters=GenerationParameters(
-                    max_tokens=None, stop_at=None, seed=SEED
-                ),
-                logits_processor=processor,
-                sampling_parameters=sampling_parameters,
-            )
-            response = ""
-            for content in tqdm(response_stream):
-                response += content
         else:
             raise ValueError(
                 "Providers are restricted to 'ollama' and 'huggingface' (uses outlines)"
