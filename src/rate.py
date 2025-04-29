6380062f704b51380306e05fff40ef86a719319f
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-04-29 12:59:20 +0100

    included support for outlines + huggingface

--- src/rate.py
+++ src/rate.py
@@ -1,3 +1,12 @@
+import outlines.processors
+import outlines.samplers
+from pyperclip import is_available
+
+
+TEMPERATURE = 0.0
+TOP_P = 0.001
+SEED = 42
+
 if __name__ == "__main__":
     import os
     import argparse
@@ -7,7 +16,7 @@ if __name__ == "__main__":
     from tqdm import tqdm
 
     from metrics import load_config
-    from ratings import Metrics
+    from ratings import Metrics, export_model_to_json
     from prompt import make_prompt
 
     parser = argparse.ArgumentParser()
@@ -15,7 +24,7 @@ if __name__ == "__main__":
     parser.add_argument("--output_path", type=str, default=None)
     parser.add_argument("--overwrite", action="store_true")
     parser.add_argument("--config_path", type=str, default="config.yaml")
-    parser.add_argument("--ollama_model", type=str, default=None)
+    parser.add_argument("--local_model", type=str, default=None)
     args = parser.parse_args()
 
     with open(args.article_path) as f:
@@ -30,14 +39,14 @@ if __name__ == "__main__":
     prompt = make_prompt(item_list, conditions)
 
     start_time = time.time()
-    if args.ollama_model is None:
+    if args.local_model is None:
         from google import genai
         from google.genai.types import GenerateContentConfig
 
         config = GenerateContentConfig(
-            temperature=0.0,
-            top_p=0.001,
-            seed=42,
+            temperature=TEMPERATURE,
+            top_p=TOP_P,
+            seed=SEED,
             response_mime_type="application/json",
             response_schema=Metrics,
         )
@@ -50,23 +59,83 @@ if __name__ == "__main__":
         )
         response = response.text
     else:
-        from ollama import Client
+        split_model = args.local_model.split(":")
+        provider, model_str = split_model[0], ":".join(split_model[1:])
+        if provider == "ollama":
+            from ollama import Client
 
-        client = Client()
-        response_stream = client.generate(
-            prompt=prompt.format(article=article_text),
-            model=args.ollama_model,
-            options={
-                "top_p": 0.001,
-                "seed": 42,
-                "temperature": 0.0,
-            },
-            format=Metrics.model_json_schema(),
-            stream=True,
-        )
-        response = ""
-        for content in tqdm(response_stream):
-            response += content["response"]
+            client = Client()
+            response_stream = client.generate(
+                prompt=prompt.format(article=article_text),
+                model=model_str,
+                options={
+                    "top_p": TOP_P,
+                    "seed": SEED,
+                    "temperature": TEMPERATURE,
+                },
+                format=export_model_to_json(),
+                stream=True,
+            )
+            response = ""
+            for content in tqdm(response_stream):
+                response += content["response"]
+        elif provider == "huggingface":
+            import outlines
+            import torch
+            from transformers import (
+                AutoModelForCausalLM,
+                AutoTokenizer,
+            )
+            from outlines.models.transformers import Transformers
+            from outlines.generate.api import (
+                GenerationParameters,
+                SamplingParameters,
+            )
+            from transformers import set_seed
+
+            if torch.cuda.is_available():
+                device = "cuda"
+            elif torch.mps.is_available():
+                device = "mps"
+            else:
+                device = "cpu"
+
+            set_seed(SEED)
+
+            model = AutoModelForCausalLM.from_pretrained(
+                model_str,
+                device_map=device,
+                torch_dtype="auto",
+                trust_remote_code=True,
+            )
+            tokenizer = AutoTokenizer.from_pretrained(model_str)
+            outlines_tokenizer = outlines.models.TransformerTokenizer(tokenizer)
+
+            processor = outlines.processors.JSONLogitsProcessor(
+                Metrics, tokenizer=outlines_tokenizer
+            )
+            sampling_parameters = SamplingParameters(
+                sampler="greedy",
+                top_p=TOP_P,
+                temperature=TEMPERATURE + 1e-6,
+            )
+
+            outlines_model = Transformers(model=model, tokenizer=tokenizer)
+            response_stream = outlines_model.stream(
+                prompt.format(article=article_text),
+                generation_parameters=GenerationParameters(
+                    max_tokens=None, stop_at=None, seed=SEED
+                ),
+                logits_processor=processor,
+                sampling_parameters=sampling_parameters,
+            )
+            response = ""
+            for content in tqdm(response_stream):
+                response += content
+        else:
+            raise ValueError(
+                "Providers are restricted to 'ollama' and 'huggingface' (uses outlines)"
+            )
 
     verbose_eval = response
     json_response = Metrics.model_validate_json(verbose_eval).model_dump(
