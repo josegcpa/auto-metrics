fd0c02e5cdd6f4edd2092a96c3740cc167faaa3b
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-04-30 16:25:31 +0100

    no longer including env in run-all, strict mode

--- run-all.sh
+++ run-all.sh
@@ -1,13 +1,17 @@
-source .env
+set -e
 
 declare -a CONFIGS=("config.yaml" "config-enhanced.yaml")
 declare -a OLLAMA_MODELS=(
     "gemma3:4b-it-q4_K_M"
     "gemma3:12b-it-q4_K_M"
+    "gemma3:27b-it-q4_K_M"
     "llama3.2:3b-instruct-q4_K_M"
     "qwen3:4b-q4_K_M"
     "qwen3:8b-q4_K_M"
+    "qwen3:14b-q4_K_M"
+    "qwen3:32b-q4_K_M"
     "mistral:7b-instruct-q4_0"
+    "mistral-small:24b-instruct-2501-q4_K_M"
     "phi4:14b-q4_K_M")
 ARTICLE_PATH=article-text
 OUTPUT_PATH=ratings
@@ -27,7 +31,7 @@ do
         do 
             text_name=$(basename "$text" | sed 's/\.txt//')
             echo Analysing $text_name with $config
-            uv run python src/rate.py \
+            echo uv run python src/rate.py \
                 --article_path "$text" \
                 --output_path "$OUTPUT_PATH/gemini/$set_name/$text_name.json" \
                 --config_path $config &
@@ -53,13 +57,20 @@ do
             for text in article-text/$set_name/*
             do 
                 text_name=$(basename "$text" | sed 's/\.txt//')
-                echo Analysing $text_name with $config
-                uv run python src/rate.py \
-                    --article_path "$text" \
-                    --output_path $OUTPUT_PATH/$(echo $model | sed 's/:/-/g')/$set_name/$text_name.json \
-                    --config_path $config \
-                    --local_model $model
+                output_path="$OUTPUT_PATH/$(echo $model | sed 's/:/-/g')/$set_name/$text_name.json"
+                if [[ ! -f "$output_path" ]]
+                then
+                    echo Analysing $text_name with $model and $config
+                    uv run python src/rate.py \
+                        --article_path "$text" \
+                        --output_path "$output_path" \
+                        --config_path $config \
+                        --local_model ollama:$model \
+                        --with_names
+                fi
             done
         done
     done
+    # stop model to free memory
+    ollama stop $model
 done
