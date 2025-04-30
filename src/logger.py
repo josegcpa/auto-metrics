94f41e1541049bf43081126800bd78393266fc0e
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-04-30 19:36:43 +0100

    added logging

---
+++ src/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+logger = logging.getLogger("Auto-METRICS")
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    fmt="%(name)s %(asctime)s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
