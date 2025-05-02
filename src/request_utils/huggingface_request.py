88316f185c008fa8d4df09b8f562b2aa875ba64a
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-05-02 09:30:32 +0100

    added maximum number of tokens

--- src/request_utils/huggingface_request.py
+++ src/request_utils/huggingface_request.py
@@ -25,7 +25,11 @@ set_seed(SEED)
 
 
 def query_model(
-    prompt: str, article_text: str, data_model: BaseModel, model_str: str
+    prompt: str,
+    article_text: str,
+    data_model: BaseModel,
+    model_str: str,
+    max_tokens: int | None = None,
 ):
     query = prompt + "\n# Article text\n\n{article}".format(
         article=article_text
@@ -54,7 +58,10 @@ def query_model(
         sampling_parameters=sampling_parameters,
     )
     response = ""
-    for content in tqdm(response_stream):
+    for i, content in tqdm(enumerate(response_stream)):
         response += content
+        if max_tokens is not None:
+            if i > max_tokens:
+                break
 
     return response
