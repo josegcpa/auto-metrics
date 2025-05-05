b60027e161abbaef981dd7a56843c10c735fdfcb
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-05-05 22:48:04 +0100

    added reasoning model results, better flow control

--- run-all.sh
+++ run-all.sh
@@ -1,5 +1,8 @@
 set -e
 
+RUN_GEMINI=false
+RUN_OLLAMA=false
+RUN_REASONING=true
 declare -a CONFIGS=("config.yaml" "config-enhanced.yaml")
 declare -a OLLAMA_MODELS=(
     "gemma3:4b-it-q4_K_M"
@@ -28,33 +31,8 @@ declare -a OLLAMA_MODELS_REASONING=(
 ARTICLE_PATH=article-text
 OUTPUT_PATH=ratings
 
-for config in ${CONFIGS[@]}
-do
-    if [[  $config == "config-enhanced.yaml" ]]
-    then
-        OUTPUT_PATH=ratings_improved
-    else 
-        OUTPUT_PATH=ratings
-    fi
-    for set in article-text/*
-    do  
-        set_name=$(basename $set)
-        for text in article-text/$set_name/*
-        do 
-            text_name=$(basename "$text" | sed 's/\.txt//')
-            echo Analysing $text_name with $config
-            uv run python -m src.rate \
-                --article_path "$text" \
-                --output_path "$OUTPUT_PATH/gemini/$set_name/$text_name.json" \
-                --config_path $config &
-        done
-    done
-done
-
-for model in ${OLLAMA_MODELS[@]}
-do
-    echo Pulling ollama model $model
-    ollama pull $model
+if [[ $RUN_GEMINI == true ]]
+then
     for config in ${CONFIGS[@]}
     do
         if [[  $config == "config-enhanced.yaml" ]]
@@ -69,60 +47,94 @@ do
             for text in article-text/$set_name/*
             do 
                 text_name=$(basename "$text" | sed 's/\.txt//')
-                output_path="$OUTPUT_PATH/$(echo $model | sed 's/:/-/g')/$set_name/$text_name.json"
-                if [[ ! -f "$output_path" ]]
-                then
-                    echo Analysing $text_name with $model and $config
-                    uv run python -m src.rate \
-                        --article_path "$text" \
-                        --output_path "$output_path" \
-                        --config_path $config \
-                        --local_model ollama:$model \
-                        --with_names \
-                        --max_tokens 5000
-                fi
+                echo Analysing $text_name with $config
+                uv run python -m src.rate \
+                    --article_path "$text" \
+                    --output_path "$OUTPUT_PATH/gemini/$set_name/$text_name.json" \
+                    --config_path $config &
             done
         done
     done
-    # stop model to free memory
-    ollama stop $model
-    ollama rm $model
-done
+fi
 
-for model in ${OLLAMA_MODELS_REASONING[@]}
-do
-    echo Pulling ollama model $model
-    ollama pull $model
-    for config in ${CONFIGS[@]}
+if [[ $RUN_OLLAMA == true ]]
+then
+    for model in ${OLLAMA_MODELS[@]}
     do
-        if [[  $config == "config-enhanced.yaml" ]]
-        then
-            OUTPUT_PATH=ratings_improved
-        else 
-            OUTPUT_PATH=ratings
-        fi
-        for set in article-text/*
-        do  
-            set_name=$(basename $set)
-            for text in article-text/$set_name/*
-            do 
-                text_name=$(basename "$text" | sed 's/\.txt//')
-                output_path="$OUTPUT_PATH/$(echo $model | sed 's/:/-/g')/$set_name/$text_name.json"
-                if [[ ! -f "$output_path" ]]
-                then
-                    echo Analysing $text_name with $model and $config
-                    uv run python -m src.rate \
-                        --article_path "$text" \
-                        --output_path "$output_path" \
-                        --config_path $config \
-                        --local_model ollama:$model \
-                        --with_names \
-                        --max_tokens 5000
-                fi
+        echo Pulling ollama model $model
+        ollama pull $model
+        for config in ${CONFIGS[@]}
+        do
+            if [[  $config == "config-enhanced.yaml" ]]
+            then
+                OUTPUT_PATH=ratings_improved
+            else 
+                OUTPUT_PATH=ratings
+            fi
+            for set in article-text/*
+            do  
+                set_name=$(basename $set)
+                for text in article-text/$set_name/*
+                do 
+                    text_name=$(basename "$text" | sed 's/\.txt//')
+                    output_path="$OUTPUT_PATH/$(echo $model | sed 's/:/-/g')/$set_name/$text_name.json"
+                    if [[ ! -f "$output_path" ]]
+                    then
+                        echo Analysing $text_name with $model and $config
+                        uv run python -m src.rate \
+                            --article_path "$text" \
+                            --output_path "$output_path" \
+                            --config_path $config \
+                            --local_model ollama:$model \
+                            --with_names \
+                            --max_tokens 5000
+                    fi
+                done
             done
         done
+        # stop model to free memory
+        ollama stop $model
+        ollama rm $model
     done
-    # stop model to free memory
-    ollama stop $model
-    ollama rm $model
-done
+fi
+
+if [[ $RUN_REASONING == true ]]
+then
+    for model in ${OLLAMA_MODELS_REASONING[@]}
+    do
+        echo Pulling ollama model $model
+        ollama pull $model
+        for config in ${CONFIGS[@]}
+        do
+            if [[  $config == "config-enhanced.yaml" ]]
+            then
+                OUTPUT_PATH=ratings_improved
+            else 
+                OUTPUT_PATH=ratings
+            fi
+            for set in article-text/*
+            do  
+                set_name=$(basename $set)
+                for text in article-text/$set_name/*
+                do 
+                    text_name=$(basename "$text" | sed 's/\.txt//')
+                    output_path="$OUTPUT_PATH/$(echo $model | sed 's/:/-/g')/$set_name/$text_name.json"
+                    if [[ ! -f "$output_path" ]]
+                    then
+                        echo Analysing $text_name with $model and $config
+                        uv run python -m src.rate \
+                            --article_path "$text" \
+                            --output_path "$output_path" \
+                            --config_path $config \
+                            --local_model ollama:$model \
+                            --with_names \
+                            --max_tokens 5000
+                    fi
+                done
+            done
+        done
+        # stop model to free memory
+        ollama stop $model
+        ollama rm $model
+    done
+fi
